5d184172142f7b3edbe74ac9e72de40ed10de7c4
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-02-10 10:59:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.37%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++) {
+            if(isdigit(s[i])) {
+                ans.pop_back();
+                continue;
+            } 
+
+            ans += s[i];
+        }
+        return ans;
+    }
+};
