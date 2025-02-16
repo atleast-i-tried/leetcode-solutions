898ee536da69dec358192871f7026b9a9f7af8a4
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-02-16 22:27:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.7%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    bool helper(int i, int n, vector<int>& ans, vector<bool>& vis) {
-        if (i >= ans.size()) {
-            return true;
-        }
+    bool helper(int i, int n, vector<int>& ans, vector<int>& vis) {
+        if (i == ans.size()) return true;
 
-        if (ans[i] != -1) {
-            return helper(i + 1, n, ans, vis);
-        }
+        if (ans[i] != -1) return helper(i + 1, n, ans, vis);
 
-        for (int curnum = n; curnum >= 1; curnum--) {  
-            if (!vis[curnum]) {
-                vis[curnum] = true;
-                ans[i] = curnum;
+        for (int num = n; num > 0; num--) {
+            if (!vis[num]) {
+                vis[num] = 1;
+                ans[i] = num;
 
-                if (curnum == 1) {
+                if (num == 1) {
                     if (helper(i + 1, n, ans, vis)) return true;
                 } else {
-                    int j = i + curnum;  
+                    int j = i + num;
                     if (j < ans.size() && ans[j] == -1) {
-                        ans[j] = curnum;
+                        ans[j] = num;
                         if (helper(i + 1, n, ans, vis)) return true;
-                        ans[j] = -1;  
+                        ans[j] = -1;   
                     }
                 }
 
-                // Backtrack by undoing
-                vis[curnum] = false;
+                vis[num] = 0;  
                 ans[i] = -1;
             }
         }
@@ -35,7 +30,8 @@ public:
 
     vector<int> constructDistancedSequence(int n) {
         vector<int> ans(2 * n - 1, -1);
-        vector<bool> vis(n + 1, false);
+        vector<int> vis(n + 1, 0);
+
         helper(0, n, ans, vis);
         return ans;
     }
