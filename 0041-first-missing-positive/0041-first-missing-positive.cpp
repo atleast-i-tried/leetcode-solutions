9455e2c731ed992acbb5bbb4699f5d8c8550e55c
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-02-21 12:51:57 +0530

    Time: 4 ms (44.7%), Space: 54.9 MB (93.54%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+
+
+// cyclic sort
+// keep swapping till a element reach its desired position, and then traverse to see which element isnt in their desired postion, return that element as answer
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
 
-        for(int i = 0; i < n; i++) {
-            if(nums[i] <= 0 || nums[i] > n) {
-                nums[i] = n + 1;
-            }
-        }
+        // for(int i = 0; i < n; i++) {
+        //     if(nums[i] <= 0 || nums[i] > n) {
+        //         nums[i] = n + 1;
+        //     }
+        // }
 
         for(int i = 0; i < n; i++) {
             while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
