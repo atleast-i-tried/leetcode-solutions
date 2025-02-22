70768e172e546c46325a13178e0eb404f05757e2
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-02-23 00:25:40 +0530

    Time: 0 ms (100%), Space: 46.1 MB (78.75%) - LeetHub

---
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        // basically we need to think of it as a pipe, remove the defected part
+
+        int right = 0;
+        int left = 0;
+        int minScore = INT_MAX;
+        int totalSum = 0;
+        int score = 0;
+        int size = cardPoints.size() - k;
+
+        for(int i: cardPoints) totalSum += i;
+
+        for(right = 0; right < cardPoints.size(); right++) {
+
+            score += cardPoints[right];
+
+            if((right - left + 1) == size) {
+                minScore = min(score, minScore);
+                score -= cardPoints[left];
+                left++;
+            }
+        }
+        return k == cardPoints.size() ? totalSum : totalSum - minScore;
+    }
+};
