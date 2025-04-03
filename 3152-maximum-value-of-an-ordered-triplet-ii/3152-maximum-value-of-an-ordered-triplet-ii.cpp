530d9bd9f41d2e4c020baea32dd7bc645506f225
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-04-04 01:10:27 +0530

    Time: 3 ms (69.57%), Space: 91 MB (90.14%) - LeetHub

---
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -0,0 +1,22 @@
+ 
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& arr) {
+        long long max_num = 0;
+        long long max_diff = 0;
+        long long ans = 0;
+
+        for(int i = 0; i < arr.size(); i++) {
+            if(i >= 2) {
+                ans = max(ans, max_diff * arr[i]);
+            }
+
+            if(i >= 1) {
+                max_diff = max(max_diff, max_num - arr[i]);
+            }
+            max_num = max(max_num, (long long)arr[i]); 
+        }
+        return ans;
+    }
+};
