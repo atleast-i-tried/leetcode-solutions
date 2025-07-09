9495a2c24a72e2537fa4bcddb077d9b95210b0c0
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-07-09 17:14:18 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.04%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int robbing(vector<int>& nums, int start, int end, vector<int> &dp) {
-        if(end == start) return nums[start];
-        if(end < start) return 0;
-        if(dp[end] != -1) return dp[end];
-
-        int pick = nums[end] + robbing(nums, start, end - 2, dp);
-        int notPick = robbing(nums, start, end - 1, dp);
-
-        return dp[end] = max(pick, notPick);
-    }
-
     int rob(vector<int>& nums) {
-        int n = nums.size(); 
-        if(n == 1) return nums[0];
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+
         vector<int> dp1(n, -1);
         vector<int> dp2(n, -1);
-        int ans1  = robbing(nums, 0, n - 2, dp1);
-        int ans2  = robbing(nums, 1, n - 1, dp2);
-        return max(ans1, ans2);
+
+        dp1[0] = nums[0];
+        dp1[1] = max(nums[0], nums[1]);
+        dp2[1] = nums[1];
+
+        if (n > 2) dp2[2] = max(nums[1], nums[2]);
+
+        for (int i = 2; i <= n - 2; i++) {
+            int pick = nums[i] + dp1[i - 2];
+            int notPick = dp1[i - 1];
+            dp1[i] = max(pick, notPick);
+        }
+
+        for (int i = 3; i <= n - 1; i++) {
+            int pick = nums[i] + dp2[i - 2];
+            int notPick = dp2[i - 1];
+            dp2[i] = max(pick, notPick);
+        }
+
+        return max(dp2[n - 1], dp1[n - 2]);
     }
 };
