e85c5d2f8cc3b2b891abf8b01c2a1e9b01864283
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-07-10 01:34:18 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-
-    int helper(int row, int col, vector<vector<int>>& dp) {
-        if(row == 0 && col == 0) return 1;
-        if(row < 0 || col < 0) return 0;
-
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int left = helper(row - 1, col, dp);
-        int right = helper(row, col - 1, dp);
-
-        return dp[row][col] = left + right;
-    }
-
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        return helper(m - 1, n - 1, dp);
+
+        //base case
+        dp[0][0] = 1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i < m && j < n) {
+                    int left = 0, right = 0;
+                    if(i == 0 && j == 0) continue;
+                    if(i > 0) left = dp[i - 1][j];
+                    if(j > 0) right = dp[i][j - 1];
+
+                    dp[i][j] = left + right;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
+
+// class Solution {
+// public:
+//     int helper(int row, int col, vector<vector<int>>& dp) {
+//         if(row == 0 && col == 0) return 1;
+//         if(row < 0 || col < 0) return 0;
+
+//         if(dp[row][col] != -1) return dp[row][col];
+
+//         int left = helper(row - 1, col, dp);
+//         int right = helper(row, col - 1, dp);
+
+//         return dp[row][col] = left + right;
+//     }
+
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int> (n, -1));
+//         return helper(m - 1, n - 1, dp);
+//     }
+// };
