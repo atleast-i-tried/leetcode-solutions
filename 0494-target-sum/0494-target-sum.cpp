58b3e0cc86a8b359d4f2f3e901426d672a195910
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-07-26 02:17:20 +0530

    Time: 410 ms (23.81%), Space: 11.7 MB (83.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& arr, int d) {
-        int totalSum = accumulate(arr.begin(), arr.end(), 0);
-        if ((totalSum + d) % 2 != 0 || (totalSum + d) < 0) return 0;
-
-        int target = (totalSum + d) / 2;
-        int n = arr.size();
-
-        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
-
-        dp[0][0] = (arr[0] == 0) ? 2 : 1;  // pick or not pick
-        if (arr[0] != 0 && arr[0] <= target) dp[0][arr[0]] = 1;
-
-        for (int i = 1; i < n; ++i) {
-            for (int t = 0; t <= target; ++t) {
-                int notPick = dp[i - 1][t];
-                int pick = (arr[i] <= t) ? dp[i - 1][t - arr[i]] : 0;
-                dp[i][t] = pick + notPick;
-            }
+    int helper(int index, vector<int>& nums, int target) {
+        if(index == 0) {
+            return (nums[0] == target) + (nums[0] == -target);
         }
 
-        return dp[n - 1][target];
+        int left = helper(index - 1, nums, target + nums[index]);
+        int right = helper(index - 1, nums, target - nums[index]);
+
+        return left + right;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        return helper(n - 1, nums, target);
     }
 };
