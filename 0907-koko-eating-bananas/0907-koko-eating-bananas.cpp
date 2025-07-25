f671e0bfed87e72092afc65d5bee4510d0c398c6
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-07-26 02:20:59 +0530

    Time: 5 ms (82.22%), Space: 22.8 MB (96.39%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
 
+
     bool possible(vector<int>& piles, int mid, int h) {
-        for (int i = 0; i < piles.size(); i++) {
-            h -= (piles[i] / mid) + (piles[i] % mid != 0);
-            if (h < 0) return false; 
+        for(int i = 0; i < piles.size(); i++) {
+            h -= piles[i] % mid ? (piles[i] / mid) + 1 : piles[i] / mid;
         }
-        return true;
+        return h >= 0;
     }
 
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (possible(piles, mid, h)) {
-                right = mid; 
+        int low = 1;
+        int high = 1000000000;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            if(possible(piles, mid, h)) {
+                high = mid - 1;
             } else {
-                left = mid + 1; 
+                low = mid + 1;
             }
         }
-        return left;
+        return low;
     }
 };
