c2ae5cd456d2a3b18d5427fc4d1ecb7e157192b2
atleast-i-tried <142111919+atleast-i-tried@users.noreply.github.com>
2025-07-27 23:18:36 +0530

    Time: 0 ms (100%), Space: 9.2 MB (69.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -8,14 +8,14 @@ public:
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(i < m && j < n) {
-                    int left = 0, right = 0;
-                    if(i == 0 && j == 0) continue;
-                    if(i > 0) left = dp[i - 1][j];
-                    if(j > 0) right = dp[i][j - 1];
+                
+                int left = 0, right = 0;
+                if(i == 0 && j == 0) continue;
+                if(i > 0) left = dp[i - 1][j];
+                if(j > 0) right = dp[i][j - 1];
 
-                    dp[i][j] = left + right;
-                }
+                dp[i][j] = left + right;
+                
             }
         }
         return dp[m - 1][n - 1];
