9f33c6dc1b76154ac9c24e41abbe6af8fb282931
Abhinav Gupta <142111919+atleast-i-tried@users.noreply.github.com>
2025-08-18 15:53:21 +0530

    Time: 48 ms (31.4%), Space: 48.9 MB (39.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,47 @@
+// class Solution {
+// public:
+//     int helper(int index, vector<int>& coins, int amount,vector<vector<int>>& dp) {
+//         if(index == 0) {
+//             if(amount % coins[0] == 0) return amount / coins[0];
+//             return 1e9;
+//         } 
+//         if(dp[index][amount] != -1) return dp[index][amount];
+//         int pick = 1e9;
+//         if(coins[index] <= amount) pick = 1 + helper(index, coins, amount - coins[index], dp);
+//         int notPick = helper(index - 1, coins, amount, dp);
+//         return dp[index][amount] = min(pick, notPick);
+//     }
+//     int coinChange(vector<int>& coins, int amount) {
+//         int n = coins.size();
+//         if(amount == 0) return 0;
+//         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+//         sort(coins.begin(), coins.end());
+//         int ans = helper(n - 1, coins, amount, dp);
+//         return ans == 1e9 ? -1 : ans;
+//     }
+// };  
 class Solution {
 public:
-    int helper(int index, vector<int>& coins, int amount, vector<vector<int>>& dp) {
-        // base case
-        if(index < 0) return 1e9;
-        if(amount == 0) return 0;
-
-        if(dp[index][amount] != -1) return dp[index][amount];
-        
-        int pick = 1e9;
-        if(coins[index] <= amount) pick = 1 + helper(index, coins, amount - coins[index], dp);
-        int notPick = helper(index - 1, coins, amount, dp);
-
-        return dp[index][amount] = min(pick, notPick);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
         if(amount == 0) return 0;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
         sort(coins.begin(), coins.end());
-        
 
-        int ans = helper(n - 1, coins, amount, dp);
+        for(int a = 0; a <= amount; a++) {
+            if(a % coins[0] == 0) dp[0][a] = a / coins[0];
+        }
 
-        return ans == 1e9 ? -1 : ans;
+        for(int i = 1; i < n; i++) {
+            for(int a = 0; a <= amount; a++) {
+                int pick = 1e9;
+                if(coins[i] <= a) pick = 1 + dp[i][a - coins[i]];
+                int notPick = dp[i - 1][a];
+
+                dp[i][a] = min(pick, notPick);
+            }
+        }
+
+        return dp[n - 1][amount] == 1e9 ? -1 : dp[n - 1][amount];
     }
 };
